b2071aa7cc9b567a90ce0e50f8fb72eab87af924
alpa99 <m_alper@hotmail.de>
2017-07-26 20:59:59 +0200

    geht alles

---
+++ shisha/shisha/SearchTVC.swift
@@ -0,0 +1,124 @@
+//
+//  SearchTVC.swift
+//  shisha
+//
+//  Created by Alper Maraz on 26.07.17.
+//  Copyright © 2017 AM. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class SearchTVC: UITableViewController {
+
+    let cellID = "cellID"
+    
+    var bars = [BarInfos] ()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchBars()
+        
+    }
+    
+    func fetchBars () {
+        var ref: DatabaseReference!
+        ref = Database.database().reference()
+        ref.child("Barliste").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject]{
+                let bar = BarInfos(dictionary: dictionary)
+                bar.setValuesForKeys(dictionary)
+                print(bar.Name!, bar.Stadt!)
+                self.bars.append(bar)
+                
+                DispatchQueue.main.async(execute: {
+                    self.tableView.reloadData()
+                } )
+            }
+            
+              //  print(snapshot)
+            
+            
+            
+            
+        }, withCancel: nil)
+        
+        
+    }
+
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+/*
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+*/
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+        return bars.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       // let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellID)
+        let bar = bars[indexPath.row]
+        
+        
+        cell.textLabel?.text = bar.Name
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
