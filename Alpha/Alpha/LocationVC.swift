70aea8d0b043d165b05f4210ba76a6f4377fae08
Ibojaa <ibe2008@hotmail.de>
2017-10-20 01:11:55 +0200

    nbnb

--- Alpha/Alpha/LocationVC.swift
+++ Alpha/Alpha/LocationVC.swift
@@ -7,29 +7,132 @@
 //
 
 import UIKit
+import Firebase
+import MapKit
+import Pulley
 
-class LocationVC: UIViewController {
+class LocationVC: UIViewController, MKMapViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBOutlet weak var map: MKMapView!
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // was ist das
+    let cellID2 = "cellID2"
+    
+    let regionRadius: CLLocationDistance = 500
+    
+    let locationManager = CLLocationManager()
+    
+    var barPointSubtitle = "shishabar sonst was"
+    
+    var BarAdressen = [String]()
+    var BarNamen = [String]()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        self.navigationController?.setNavigationBarHidden(true, animated: true)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        map.delegate = self
+        fetchAdress()
     }
-    */
-
+    
+    func fetchAdress() {
+        
+        var datref: DatabaseReference!
+        datref = Database.database().reference()
+        datref.child("BarInfo").observe(.childAdded, with: { (snapshot) in
+            
+            if let dictionary = snapshot.value as? [String: AnyObject]{
+                let bars = BarInfos(dictionary: dictionary)
+                self.BarAdressen.append(bars.Adresse!)
+                self.BarNamen.append(bars.Name!)
+                for BarIndex in 0 ..< self.BarAdressen.count {
+                    self.getPlaceMarkFromAdress(adress: self.BarAdressen[BarIndex], Titlex: self.BarNamen[BarIndex])
+                    
+                }
+            }
+            
+        }, withCancel: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool){
+        locationAuthStatus()
+    }
+    
+    func locationAuthStatus(){
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse{
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func centerMapOnLocation(location:CLLocation){
+        
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, regionRadius * 2 , regionRadius * 2)
+        map.setRegion(coordinateRegion, animated: true)
+        
+        
+    }
+    
+    
+    func createAnnotationForLocation(location: CLLocation, Title: String){
+        
+        
+        let barpoint = BarAnnotation(coordinate: location.coordinate, title: Title, subtitle: barPointSubtitle)
+        map.addAnnotation(barpoint)
+    }
+    
+    
+    func getPlaceMarkFromAdress (adress: String, Titlex: String){
+        CLGeocoder().geocodeAddressString(adress){ (placemarks: [CLPlacemark]?,error: Error?) -> Void in
+            if let marks = placemarks, marks.count > 0 {
+                if let loc = marks[0].location {
+                    self.createAnnotationForLocation(location: loc, Title: Titlex)}
+                
+            }
+        }
+        
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        if annotation is MKUserLocation {return nil}
+        
+        let reuseId = "pin"
+        
+        var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseId) as? MKPinAnnotationView
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.animatesDrop = true
+            let calloutButton = UIButton(type: .detailDisclosure)
+            pinView!.rightCalloutAccessoryView = calloutButton
+            pinView!.sizeToFit()
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        
+        return pinView
+    }
+    
+    
+   /* func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView{
+            let annotation = self.map.selectedAnnotations[0] as MKAnnotation!
+            //Perform a segue here to navigate to another viewcontroller
+            
+            let detvc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "BarDetailVC") as! BarDetailVC
+            detvc.barname = ((annotation?.title)!)!
+            
+            (parent as? PulleyViewController)?.setDrawerPosition(position: PulleyPosition(rawValue: 2)!)
+            
+            
+            (parent as? PulleyViewController)?.setDrawerContentViewController(controller: detvc, animated: true)
+            
+        }*/
 }
