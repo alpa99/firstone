0ada4306d812d54e58dbd8e1a65cb08f02e64916
Ibojaa <ibe2008@hotmail.de>
2017-10-23 15:47:16 +0200

    mn

--- Alpha/Pods/Target Support Files/Pods-AlphaTests/Pods-AlphaTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AlphaTests : NSObject
-@end
-@implementation PodsDummy_Pods_AlphaTests
-@end
--- Alpha/Pods/Target Support Files/Pods-AlphaUITests/Pods-AlphaUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AlphaUITests : NSObject
-@end
-@implementation PodsDummy_Pods_AlphaUITests
-@end
