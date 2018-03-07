72324265d00c6dc59e66523c2009ade4e4f94587
Ibojaa <ibe2008@hotmail.de>
2018-03-07 23:21:42 +0100

    nkkk

--- Alpha/Alpha/pulley/DetailVC.swift
+++ Alpha/Alpha/pulley/DetailVC.swift
@@ -22,9 +22,10 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
     var adresse = String ()
     var picture = String ()
     var parentPageViewController: PageViewController!
-    var sdWebImageSource = [SDWebImageSource]()
+    var sdWebImageSource2 = [SDWebImageSource]()
 
-    
+    let sdWebImageSource = [SDWebImageSource(urlString: "https://images.unsplash.com/photo-1432679963831-2dab49187847?w=1080"), SDWebImageSource(urlString: "https://images.unsplash.com/photo-1447746249824-4be4e1b76d66?w=1080"), SDWebImageSource(urlString: "https://images.unsplash.com/photo-1463595373836-6e0b0a8ee322?w=1080")]
+
     func getParentPageViewController(parentRef: PageViewController) {
         parentPageViewController = parentRef
         
@@ -32,24 +33,24 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
 
         self.barname = parentPageViewController.name
-        self.Namelbl.text = self.barname
-
+//        fetchPicsCount2()
         fetchPicsCount()
+//        fetchpics()
         fetchData()
         slideshow.backgroundColor = UIColor.white
+ 
         slideshow.slideshowInterval = 0.0
         slideshow.pageControlPosition = PageControlPosition.insideScrollView
-        slideshow.pageControl.currentPageIndicatorTintColor = UIColor(red: 185.0/255.0, green: 170.0/255.0, blue: 140.0/255.0, alpha: 1.0)
-        slideshow.pageControl.pageIndicatorTintColor = UIColor(red: 90.0/255.0, green: 90.0/255.0, blue: 90.0/255.0, alpha: 1.0)
+        slideshow.pageControl.currentPageIndicatorTintColor = UIColor(red: 255.0/255.0, green: 255.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+        slideshow.pageControl.pageIndicatorTintColor = UIColor(red: 185.0/255.0, green: 170.0/255.0, blue: 140.0/255.0, alpha: 1.0)
    
         
         slideshow.contentScaleMode = UIViewContentMode.scaleAspectFill
         
        
-       
+        print(sdWebImageSource, "source111")
         let recognizer = UITapGestureRecognizer(target: self, action: #selector(DetailVC.didTap))
         slideshow.addGestureRecognizer(recognizer)
     }
@@ -70,7 +71,28 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
     @IBAction func Backbtn(_ sender: UIButton) {
        parentPageViewController.goback()
         
+     // performSegue(withIdentifier: "drawervc", sender: self)
     }
+    
+//    func fetchPicsCount2(){
+//        var refd: DatabaseReference!
+//        refd = Database.database().reference()
+//        refd.child("BarInfo").child("\(barname)").child("Bilder").observe(.value, with: { (snapshot) in
+//            print(snapshot.childrenCount, "SNAPZZZZ")
+////            if let dictionary = snapshot.value as? [String: AnyObject]{
+////                let counter = Bilder(dictionary: dictionary)
+////                    self.counterlink.append(counter.link!)
+////
+////                print(self.counterlink.count, "COUNTERLINK")
+////                //                print(bild, "bild")
+////                //                self.bilder.append(bild.link!)
+////
+////
+////            }
+//
+//        } , withCancel: nil)
+//    }
+    
     
     
     func fetchPicsCount(){
@@ -97,9 +119,9 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
 
                 for links in self.bilder{
 
-                    self.sdWebImageSource.append(SDWebImageSource(urlString: links)!)
+                    self.sdWebImageSource2.append(SDWebImageSource(urlString: links)!)
 
-                    self.slideshow.setImageInputs(self.sdWebImageSource as [InputSource])
+                    self.slideshow.setImageInputs(self.sdWebImageSource2 as [InputSource])
                     
                     }
                 }
@@ -115,6 +137,7 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
         
         print("vgv", barname)
         var ref: DatabaseReference!
+        self.Namelbl.text = self.barname
         ref = Database.database().reference()
         ref.child("BarInfo").child("\(barname)").observe(.value, with: { (snapshot) in
             
@@ -122,6 +145,35 @@ class DetailVC: UIViewController, PulleyDrawerViewControllerDelegate, PageObserv
                 let bar = BarInfos(dictionary: dictionary)
                 
                 self.bars.append(bar)
+                self.picture.append(bar.Bild!)
+                print(self.picture)
+
+                let storageRef = Storage.storage().reference(forURL: "\(self.picture)")
+                
+//                storageRef.downloadURL(completion: {(url, error) in
+//
+//                    if error != nil {
+//                        print(error?.localizedDescription ?? "error")
+//                        return
+//                    }
+//
+//                    URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+//
+//                        if error != nil {
+//                            print(error ?? "error")
+//                            return
+//                        }
+//
+//                        guard let imageData = UIImage(data: data!) else { return }
+//
+//                        DispatchQueue.main.async {
+//                            self.image.image = imageData
+//                        }
+//
+//                }).resume()
+//                })
+                
+
 
             }} , withCancel: nil)
         
