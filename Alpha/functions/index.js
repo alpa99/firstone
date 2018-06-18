dab4b774bf175bdb3ad467f2f446bb4b2db57c2f
Alper Maraz <m_alper@hotmail.de>
2018-06-18 22:18:33 +0200

    df

--- Alpha/functions/index.js
+++ Alpha/functions/index.js
@@ -3,25 +3,9 @@ const admin = require('firebase-admin');
 
 admin.initializeApp(functions.config().firebase);
 // database tree
-exports.sendNewBestellung = functions.database.ref('/Bestellungen/{id}').onWrite(event=>{
-    const uuid = event.params.id;
-                                                                                         
-    console.log('User to send notification', uuid);
-                                                                                         
-    var ref = admin.database().ref(`Users/${uuid}/token`);
-    return ref.once("value", function(snapshot){
-                                                                                                         
-    const payload = {
-    notification: {
-    title: 'Neue Bestellung',
-    body: 'Tap here to check it out!'
-    }
-    };
-                                                                                                         
-    admin.messaging().sendToDevice(snapshot.val(), payload)
-                                                                                                         
-    },
-    function (errorObject) {
-    console.log("The read failed: " + errorObject.code);
-    });
+exports.sendNewBestellung = functions.database.ref('/userBestellungen/{UID}').onWrite(event=>{
+        let projectData = event.data.val();
+        if (!event.data.previous.exists()) {
+        // Do things here if project didn't exists before
+        }
 })
