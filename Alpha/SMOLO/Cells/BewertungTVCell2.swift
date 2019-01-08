49a511cda9013d9d86afe967b9f26b6af919da7d
Alper Maraz <m_alper@hotmail.de>
2019-01-08 21:35:31 +0100

    mk

--- Alpha/SMOLO/Cells/BewertungTVCell2.swift
+++ Alpha/SMOLO/Cells/BewertungTVCell2.swift
@@ -7,15 +7,23 @@
 //
 
 import UIKit
+import Cosmos
 
 class BewertungTVCell2: UITableViewCell {
     
     @IBOutlet weak var ItemLbl: UILabel!
    
+    @IBOutlet weak var Cosmos: CosmosView!
+    @IBOutlet weak var Abschicken: UIButton!
     
     
+    @IBAction func Absenden(_ sender: UIButton) {
+        print(self.note, "note")
+
+    }
     var section2 = Int()
     var row2 = Int()
+    var note = 0.0
 //    var delegate: bestellenCell2Delegate?
 //
 //
@@ -25,6 +33,13 @@ class BewertungTVCell2: UITableViewCell {
 //
 
     override func awakeFromNib() {
+        
+        Cosmos.settings.fillMode = .half
+        Cosmos.didTouchCosmos = { rating in
+            self.note = rating
+        }
+        Cosmos.settings.starSize = 23.0
+        Cosmos.settings.starMargin = 5.0
         super.awakeFromNib()
         ItemLbl.font = UIFont(name: "Verdana", size: 15.0)
         // Initialization code
